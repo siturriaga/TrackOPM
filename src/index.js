fb54687284036c5c83cc8852c04ff59b27d749a5
siturriaga <iturriaga.sebastian.e@gmail.com>
2025-10-05 17:57:49 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx'; // We point to our new App.jsx file
-
-// Basic global styles can go here
-document.body.style.margin = '0';
-document.body.style.fontFamily = 'sans-serif';
-document.body.style.backgroundColor = '#f4f7fa';
+import App from './App.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
