d89a180b58947274e18e4334a2c58258e7c956f9
siturriaga <iturriaga.sebastian.e@gmail.com>
2025-10-11 12:17:19 -0400

    Update firebaseClientConfig.js

--- netlify/functions/firebaseClientConfig.js
+++ netlify/functions/firebaseClientConfig.js
@@ -1,31 +1,32 @@
-// Netlify Function: returns your Firebase Web config (public client config).
-// Inline values avoid env var issues and unblock Firebase initialization.
+export const handler = async (event, context) => {
+    if (event.httpMethod === 'OPTIONS') {
+        return {
+            statusCode: 204,
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET, OPTIONS',
+                'Access-Control-Allow-Headers': 'Content-Type',
+            },
+            body: ''
+        };
+    }
 
-function json(status, body) {
-  return {
-    statusCode: status,
-    headers: {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Headers": "Content-Type, Authorization",
-      "Access-Control-Allow-Methods": "GET, OPTIONS"
-    },
-    body: JSON.stringify(body)
-  };
-}
+    const config = {
+        apiKey: process.env.FIREBASE_API_KEY,
+        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+        appId: process.env.FIREBASE_APP_ID,
+        measurementId: process.env.FIREBASE_MEASUREMENT_ID
+    };
 
-exports.handler = async (event) => {
-  if (event.httpMethod === 'OPTIONS') return json(200, {});
-  if (event.httpMethod !== 'GET') return json(405, { error: "Method Not Allowed" });
-
-  // 🔓 Firebase Web config is safe to expose (client SDK uses it)
-  return json(200, {
-    apiKey: "AIzaSyBT95w8fzJr9J5WCYe8iwFkvSrwXds5sms",
-    authDomain: "trackopmn.firebaseapp.com",
-    projectId: "trackopmn",
-    appId: "1:325895934431:web:50665d95aab0ac1a4b746a",
-    storageBucket: "trackopmn.firebasestorage.app",
-    messagingSenderId: "325895934431",
-    measurementId: "G-QX53NY1CJC"
-  });
+    return {
+        statusCode: 200,
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+        },
+        body: JSON.stringify(config),
+    };
 };
