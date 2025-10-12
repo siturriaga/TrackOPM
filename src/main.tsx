f2b9a9f843aaa406e4ef8e7f24078fe889227eb6
siturriaga <iturriaga.sebastian.e@gmail.com>
2025-10-12 18:34:47 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';            // <-- imports the default export
+import './index.css';               // or './app.css' if you prefer
 
-
-createRoot(document.getElementById('root')!).render(
-<React.StrictMode>
-<App />
-</React.StrictMode>
-)
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
