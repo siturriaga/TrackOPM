be6e9d0b7b08892948f6723915fc353fb10125fb
siturriaga <iturriaga.sebastian.e@gmail.com>
2025-10-12 15:52:05 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,1 @@
-import React from 'react'
-import Header from './components/Header'
-import AuthGate from './components/AuthGate'
-import Dashboard from './components/Dashboard'
-
-
-export default function App() {
-return (
-<AuthGate>
-<Header onBack={undefined} onExit={() => window.location.assign('/')} />
-<Dashboard />
-</AuthGate>
-)
-}
+import AuthGate from './components/AuthGate';
