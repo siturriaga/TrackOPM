e5d502891debeaa48149e40d18fc276ec677e66c
siturriaga <iturriaga.sebastian.e@gmail.com>
2025-10-12 14:04:19 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
-// vite.config.js
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: "dist",
-    rollupOptions: {
-      external: [], // nothing to externalize
-    },
-  },
-});
+plugins: [react()],
+server: { port: 5173 },
+preview: { port: 4173 },
+esbuild: { legalComments: 'none' }
+})
