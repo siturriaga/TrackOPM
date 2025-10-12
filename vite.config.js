a39a8c332f920f162eddc047f692eb36f4f9b86d
siturriaga <iturriaga.sebastian.e@gmail.com>
2025-10-12 15:02:16 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: { sourcemap: false }, // helps avoid eval-like behavior in maps
   server: { port: 5173 },
   preview: { port: 4173 },
   esbuild: { legalComments: 'none' }
-})
+});
