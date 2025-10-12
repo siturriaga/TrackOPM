2a5e7c816ccacef6e9741d47b49826bf0d9e70eb
siturriaga <iturriaga.sebastian.e@gmail.com>
2025-10-12 14:37:54 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
 export default defineConfig({
-plugins: [react()],
-server: { port: 5173 },
-preview: { port: 4173 },
-esbuild: { legalComments: 'none' }
+  plugins: [react()],
+  server: { port: 5173 },
+  preview: { port: 4173 },
+  esbuild: { legalComments: 'none' }
 })
