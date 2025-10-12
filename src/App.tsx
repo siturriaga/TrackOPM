66e87e4d310526622e22b8e1b3d397cfb64b5200
siturriaga <iturriaga.sebastian.e@gmail.com>
2025-10-12 18:34:07 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,15 @@
+import React from 'react';
+import Header from './components/Header';
+import Dashboard from './components/Dashboard';
 import AuthGate from './components/AuthGate';
+
+const App: React.FC = () => {
+  return (
+    <AuthGate>
+      <Header />
+      <Dashboard />
+    </AuthGate>
+  );
+};
+
+export default App; // <-- REQUIRED
