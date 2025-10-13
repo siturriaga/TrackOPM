a432bf548ef5def1f46d621296a2b1c8c82c80f3
siturriaga <iturriaga.sebastian.e@gmail.com>
2025-10-12 21:30:42 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
-import './index.css'
+// /src/main.jsx
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
 
-const root = createRoot(document.getElementById('root')!)
-root.render(<App />)
+createRoot(document.getElementById("root")).render(<App />);
