a49d77da71450ea166a7f8efd5ef12253ce553a6
siturriaga <iturriaga.sebastian.e@gmail.com>
2025-10-12 21:31:56 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
-// /src/main.jsx
+// /src/main.tsx
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import App from "./App";
 
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root") as HTMLElement).render(<App />);
